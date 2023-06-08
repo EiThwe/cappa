efa21d714a1dd30c4d3e2d66bc087e70ef0bda3e
Raphtalia Kay <miranasiam023@gmail.com>
2023-06-08 23:37:52 +0630

    coming soon not finish

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./ComingSoon.css";
 
 const ComingSoon = () => {
-  return <div></div>;
+  return <>
+{/* first bg  */}
+    <div className=" background ">
+      {/* second bg */}
+     <div className="blackbackground relative">
+      <div className="container absolute mx-auto w-[700px] flex  h-screen border-2 ">
+        <h3 className="">Coming Soon!</h3>
+      </div>
+     </div>
+     
+    </div>
+  </>;
 };
 
 export default ComingSoon;
